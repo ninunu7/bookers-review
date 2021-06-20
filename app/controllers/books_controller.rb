0a0cacb6a5f136e24b4413702fb7034f40d50e11
ninunu7 <pibf51@gmail.com>
2021-06-20 08:26:07 +0000

    [add]error_messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :index
+    end
   end
 
   def show
@@ -18,10 +22,13 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:])
+    @book = Book.find(params[:id])
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
